2ad06ab49a14490a7215d43319b4aafc9821e167
prayas7102 <prayas.prithvirajpratap7@gmail.com>
2023-08-18 17:35:21 +0530

    cross service communication

--- UserService/src/main/java/com/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/user/service/services/impl/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.user.service.services.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import com.user.service.entity.Rating;
 import com.user.service.entity.User;
 import com.user.service.exceptions.ResourceNotFoundException;
 import com.user.service.repository.UserRepository;
@@ -17,6 +20,9 @@ public class UserServiceImpl implements UserService {
 	@Autowired
 	private UserRepository userRepository;
 
+	@Autowired
+	private RestTemplate restTemplate;
+
 	@Override
 	public User saveUser(User user) {
 		String randomUserId = UUID.randomUUID().toString();
@@ -26,12 +32,26 @@ public class UserServiceImpl implements UserService {
 
 	@Override
 	public List<User> getAllUser() {
-		return userRepository.findAll();
+		List<User> userList = userRepository.findAll();
+		for (User user : userList) {
+			@SuppressWarnings("unchecked")
+			ArrayList<Rating> ratingsOfUser = restTemplate
+					.getForObject("http://RATING-SERVICE/ratings/users/" + user.getUserId(), ArrayList.class);
+			user.setRatings(ratingsOfUser);
+		}
+		return userList;
 	}
 
 	@Override
 	public User getUser(String UserId) {
-		return userRepository.findById(UserId).orElseThrow(() -> new ResourceNotFoundException(UserId + " user not found"));
+		User user = userRepository.findById(UserId)
+				.orElseThrow(() -> new ResourceNotFoundException(UserId + " user not found"));
+
+		@SuppressWarnings("unchecked")
+		ArrayList<Rating> ratingsOfUser = restTemplate
+				.getForObject("http://RATING-SERVICE/ratings/users/" + user.getUserId(), ArrayList.class);
+		user.setRatings(ratingsOfUser);
+		return user;
 	}
 
 }
